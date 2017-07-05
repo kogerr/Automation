181396f64906d31c00c482162ad362bc12e179ee
koger <kogerr@hotmail.com>
2017-07-05 23:56:00 +0200

    started counter exercise

--- src/counter/Combination.java
+++ src/counter/Combination.java
@@ -1,5 +1,24 @@
 package counter;
 
 public class Combination {
+<<<<<<< HEAD
 
+=======
+  private Link lastLink;
+  private boolean finished;
+  
+  private Link finishedChecker = new Link(){
+
+    @Override
+    public void step() {
+      finished = true;
+    }
+
+    @Override
+    public void giveLastLink(Link last) {
+      lastLink = last;
+    }
+    
+  };
+>>>>>>> started counter exercise
 }
