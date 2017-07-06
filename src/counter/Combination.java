0a1307fb45b1378ef6d384be608496034db139e3
Gergely Kovacs3 <kogerr@hotmail.com>
2017-07-06 08:35:34 +0200

    changed Combination to array

--- src/counter/Combination.java
+++ src/counter/Combination.java
@@ -1,24 +1,41 @@
 package counter;
 
+import java.util.Arrays;
+
 public class Combination {
-<<<<<<< HEAD
+  private static final byte MIN_VALUE = 97;
+  private static final byte MAX_VALUE = 122;
+  private final byte[] combination;
+  private boolean atMax;
 
-=======
-  private Link lastLink;
-  private boolean finished;
+  Combination(int length) {
+    combination = new byte[length];
+    Arrays.fill(combination, MIN_VALUE);
+  }
   
-  private Link finishedChecker = new Link(){
-
-    @Override
-    public void step() {
-      finished = true;
+  private void step(int cursor) {
+    if (combination[cursor] < MAX_VALUE) {
+      combination[cursor]++;
+    } else {
+      if (cursor > 0) {
+        combination[cursor] = MIN_VALUE;
+        step(cursor - 1);
+      } else {
+        atMax = true;
+      }
     }
+  }
+  
+  public void step() {
+    step(combination.length - 1);
+  }
+  
+  @Override
+  public String toString() {
+    return new String(combination);
+  }
 
-    @Override
-    public void giveLastLink(Link last) {
-      lastLink = last;
-    }
-    
-  };
->>>>>>> started counter exercise
+  public boolean isAtMax() {
+    return atMax;
+  }
 }
